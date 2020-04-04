8c60aa480f0e052f9db343d4caf7e37494b96590
Marcos Antonio <56478707+m-antonio22069@users.noreply.github.com>
2020-04-04 01:46:58 -0300

    Atualização

--- Robos/tRobot.py
+++ Robos/tRobot.py
@@ -1,5 +1,9 @@
 import Algorithmia
 import spacy
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson.natural_language_understanding_v1 import Features, KeywordsOptions
+
 
 def fetchContentFromWiki(term):
     entrada = {"articleName": term, "lang": 'pt'}
@@ -26,12 +30,37 @@ def breakContentIntoSentences(cleanVar):
     sent = nlp(cleanVar)
     doc = []
     for sent in sent.sents:
-        doc.append([sent, [sent.ents], []])
+        doc.append(str(sent))
     return doc
 
+
+def fethWatsonAndReturnKeywords(doc):
+    # Autenticação de usuario
+    authenticator = IAMAuthenticator('HZWB8HPGlrXgLPOeZN9kTPpXMK2ok0EAqGdLv9dcrIsH')
+    natural_language_understanding = NaturalLanguageUnderstandingV1(
+        version='2019-07-12',
+        authenticator=authenticator
+    )
+    natural_language_understanding.set_service_url(
+        'https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/ed1586a0-30bb-4312-9206'
+        '-97c8c883d30b')
+    keyInformation = []
+    for index in range(3):
+        response = natural_language_understanding.analyze(
+            text=doc[index],
+            features=Features(keywords=KeywordsOptions(sentiment=False, emotion=False, limit=5)),
+            language='pt').get_result()
+        keyWordsTMP = response['keywords']
+        keyWords = []
+        for i in range(len(keyWordsTMP)):
+            keyWords.append(keyWordsTMP[i]['text'])
+        keyInformation.append([{'sentence': doc[index]},keyWords])
+    return keyInformation
 
 def ActivateBootText(bruteTerm):
     bruteVar = fetchContentFromWiki(bruteTerm)
     cleanVar = sanitizeContent(bruteVar)
     doc = breakContentIntoSentences(cleanVar)
-    print(doc[1])
+    keyInformation = fethWatsonAndReturnKeywords(doc=doc)
+    for i in range(len(keyInformation)):
+        print(keyInformation[i])
--- teste.py
+++ teste.py
@@ -1,18 +1,40 @@
-import json
+'''import json
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-from ibm_watson.natural_language_understanding_v1 import Features, CategoriesOptions
+from ibm_watson.natural_language_understanding_v1 import Features, KeywordsOptions
 
-authenticator = IAMAuthenticator("HZWB8HPGlrXgLPOeZN9kTPpXMK2ok0EAqGdLv9dcrIsH")
+authenticator = IAMAuthenticator('HZWB8HPGlrXgLPOeZN9kTPpXMK2ok0EAqGdLv9dcrIsH')
 natural_language_understanding = NaturalLanguageUnderstandingV1(
     version='2019-07-12',
     authenticator=authenticator
 )
 
-natural_language_understanding.set_service_url("https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/ed1586a0-30bb-4312-9206-97c8c883d30b")
+natural_language_understanding.set_service_url('https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/ed1586a0-30bb-4312-9206-97c8c883d30b')
 
 response = natural_language_understanding.analyze(
-    text='Meu nome é marcos', features=Features(categories=CategoriesOptions(limit=3))
-).get_result()
+    text='Michael Joseph Jackson (Gary, 29 de agosto de 1958 — Los Angeles, 25 de junho de 2009) foi um cantor, compositor e dançarino estadunidense. Apelidado de "Rei do Pop", ele é considerado uma das figuras culturais mais importantes do século XX e um dos maiores artistas da história da música. As contribuições de Jackson para a música, a dança e a moda, juntamente com a divulgação de sua vida pessoal, fizeram dele uma figura global na cultura popular por mais de quatro décadas.',
+    features=Features(keywords=KeywordsOptions(sentiment=False,emotion=False,limit=2))).get_result()
 
-print(json.dumps(response, indent=2))
+print(response)      '''
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson.natural_language_understanding_v1 import Features, KeywordsOptions
+doc = ['Michael Joseph Jackson (Gary, 29 de agosto de 1958 — Los Angeles, 25 de junho de 2009) foi um cantor, compositor e dançarino estadunidense. '
+       'Apelidado de "Rei do Pop", ele é considerado uma das figuras culturais mais importantes do século XX e um dos maiores artistas da história da música.',
+       'As contribuições de Jackson para a música, a dança e a moda, juntamente com a divulgação de sua vida pessoal, '
+       'fizeram dele uma figura global na cultura popular por mais de quatro décadas.']
+# Autenticação de usuario
+authenticator = IAMAuthenticator('HZWB8HPGlrXgLPOeZN9kTPpXMK2ok0EAqGdLv9dcrIsH')
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+    version='2019-07-12',
+    authenticator=authenticator
+)
+natural_language_understanding.set_service_url(
+    'https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/ed1586a0-30bb-4312-9206-97c8c883d30b')
+informations = []
+for i in doc:
+    response = natural_language_understanding.analyze(
+        text=i,
+        features=Features(keywords=KeywordsOptions(sentiment=False, emotion=False, limit=2))).get_result()
+    informations.append(response)
+print(informations)
